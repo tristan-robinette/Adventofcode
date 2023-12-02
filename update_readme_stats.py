b2b508a1c766a78f8f311d8f7ef48db4bad37da1
tristan-robinette <tristan-robinette@hotmail.ca>
2023-12-02 18:33:07 +0000

    refactor to make readme generate with dropdowns for viewing code and plugin approach when generating days

--- update_readme_stats.py
+++ update_readme_stats.py
@@ -1,110 +1,163 @@
 import os
-import sys
-import datetime
+import pathlib
 from subprocess import check_output
 import re
-import requests
-import random
 
 X_MAS_EMOJIES = [
     "👼", "⛄", "🎁", "🦌", "🎄", "🤶", "🎅", "❄️", "🌟", "🍪", "⛸️", "🔔", "✨", "🥛", "🍷",
     "🥞", "🕯", "🎶", "🧦", "🤍", "🥂", "🌿", "🍾", "🏂", "🧣", "🛷", "☕", "🍫", "👪"
 ]
-#random.shuffle(X_MAS_EMOJIES)
+
+
+ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+def get_years_to_build():
+    return [d for d in os.listdir(os.path.join(ROOT_PATH)) if d.startswith("20")]
+
+
+def get_available_days_to_process(year) -> set:
+    return {
+        int(d.split('_')[-1])
+        for d in os.listdir(os.path.join(ROOT_PATH, str(year)))
+        if d.startswith('day_')
+    }
+
+def get_readme_text(filename='readme.md'):
+    readme_file = os.path.join(ROOT_PATH, filename)
+    with open(readme_file, 'r') as f:
+        return f.read()
+
+def does_day_already_exists_for_year(readme, path) -> bool:
+    year_exists = False
+    day_exists = False
+    year = path.split("/")[-2]
+    day = path.split("/")[-1]
+    for line in readme.splitlines():
+        if year_match := re.findall(r'## Year (\d+)', line):
+            if str(year_match[0]) == str(year):
+                year_exists = True
+        if day_match := re.findall(r'## Day (\d+)', line):
+            if str(day_match[0]) == str(day):
+                day_exists = True
+    return year_exists is True and day_exists is True
+
+def get_day_existing_text(readme, path):  # sourcery skip: use-named-expression
+    year = path.split("/")[-2]
+    day = path.split("/")[-1]
+    year_index = 0
+    start_day_index = 0
+    existing_day_text = []
+    # find year boundary
+    for line in readme.splitlines():
+        year_match = re.findall(r'## Year (\d+)', line)
+        if year_match and str(year_match[0]) == str(year):
+            year_index = readme.index(line)
+            break
+    # Find first day within year boundary
+    for line in readme[year_index:].splitlines():
+        day_match = re.findall(r'Day (\d+)', line)
+        if day_match and str(day_match[0]) == str(day) and "###" in line:
+            start_day_index = readme[year_index: ].index(line)
+            break
+    # get text for that day
+    for line in readme[year_index:][start_day_index: ].splitlines():
+        if line.strip() == "<hr>":
+            existing_day_text.extend((line, ''))
+            break
+        existing_day_text.append(line)
+    return existing_day_text
+
+def get_text_for_new_day(path):
+    year = path.split("/")[-2]
+    day = path.split("/")[-1]
+    print(f"Updating day {day} {year}")
+    # get day's name
+    emoji = X_MAS_EMOJIES[int(day) % len(X_MAS_EMOJIES)]
+    result = ['', f"### {emoji} Day {day}\n"]
+    # Get each part
+    for part in range(1, 2 + 1):
+        result.append(f"#### Day {day} Solution Part {part}\n")
+        day_path = os.path.join(ROOT_PATH, str(year), f"day_{day}")
+        solution_file = os.path.join(day_path, f"solution_part_{part}.py")
+        input_file = os.path.join(day_path, "input.txt")
+        # Perform the run
+        run_output = check_output(["python3", solution_file, input_file])
+        run_output = run_output.decode('utf-8').strip()
+        result += run_output.splitlines()[-2:]
+        # Add the approach
+        with open(solution_file, 'r') as f:
+            code_text = f.read()
+        code_text = code_text[:code_text.find('if __name__ == "__main__":')].strip()
+        result.append("")
+        result.append("")
+        result.append("```python")
+        result += code_text.splitlines()
+        result.append("```")
+        result.append('')
+    result.append('<hr>')
+    return result
+
+
+def build_day(readme, year, day) -> list:
+    path_to_dir = os.path.join(ROOT_PATH, str(year), str(day))
+    if does_day_already_exists_for_year(readme, path_to_dir):
+        return get_day_existing_text(readme, path_to_dir)
+    return get_text_for_new_day(path_to_dir)
+
+def add_dropdown_to_day(day_lines:list) -> list:
+    # check if details already exists
+    if any(line.startswith("<details>") for line in day_lines):
+        return day_lines
+    # track open/close of code tag
+    found_open_tag = False
+
+    out = []
+    for line in day_lines:
+        if line.startswith("```"):
+            if not found_open_tag:
+                out += ['<details>', '<summary>View code</summary>', '']
+                found_open_tag = True
+            else:
+                out += ['```', '', '</details>', '']
+                found_open_tag = False
+                continue
+        out.append(line)
+    return out
+
+def add_toc_to_day(day_lines:list) -> list:
+    # todo implement
+    return day_lines
+
+def add_day_plugins(readme_lines:list, code_as_dropdown=True, build_toc=True) -> list:
+    if code_as_dropdown:
+        readme_lines = add_dropdown_to_day(readme_lines)
+    if build_toc:
+        readme_lines = add_toc_to_day(readme_lines)
+    return readme_lines
+
+def main(readme_template_name=None, code_as_dropdown=True, build_toc=True):
+    current_readme = get_readme_text()
+    output_readme = []
+
+    for year_to_build in get_years_to_build():
+        days_available = get_available_days_to_process(year_to_build)
+        for day in days_available:
+            if int(day) == 1:
+                output_readme += [f'### Year {year_to_build}', '']
+            readme_lines = build_day(current_readme, year_to_build, day)
+            readme_lines = add_day_plugins(readme_lines, code_as_dropdown, build_toc)
+            output_readme += readme_lines
+
+    readme_template = ''
+    if readme_template_name:
+        readme_template = pathlib.Path(
+            os.path.join(ROOT_PATH, readme_template_name)
+        ).read_text()
+    readme_file = os.path.join(ROOT_PATH, 'readme.md')
+    with open(readme_file, 'w') as f:
+        f.write(readme_template + '\n'.join(output_readme))
 
 
 if __name__ == '__main__':
-    # Get date
-    today_date = datetime.datetime.now()
-    if today_date.month != 12:
-        print("It's not December you silly goose!")
-        sys.exit()
-    current_year = today_date.year
-    current_day = today_date.day
-    
-    root_path = os.path.dirname(os.path.realpath(__file__))
-
-    # Read the README
-    readme_file = os.path.join(root_path, 'readme.md')
-    with open(readme_file, 'r') as f:
-        readme = f.read()
-        readme = readme.splitlines()
-    
-    # Read available days
-    days_available = set()
-    for d in os.listdir(os.path.join(root_path, str(current_year))):
-        if d.startswith('day_'):
-            days_available.add(int(d.split('_')[-1]))
-    print(f"all days_available:", days_available)
-    
-    # READ known days
-    days_known = set()
-    readme_year = False
-    for line in readme:
-        # find year
-        match = re.findall(r'## Year (\d+)', line)
-        if match:
-            readme_year = current_year == int(match[0])
-        # create/find day
-        match = re.findall(r'### . Day (\d+)', line)
-        if match and readme_year:
-            days_known.add(int(match[0]))
-    print(f"days_known:", days_known)
-    last_day_known = max(days_known, default=0)
-    print(f"last_day_known:", last_day_known)
-    days_available -= days_known
-    print(f"unknown days_available:", days_available)
-    if len(days_available) == 0:
-        print(f"No new solutions found")
-        sys.exit()
-    
-    # Fill in the README
-    result = []
-    readme_year = False
-    readme_day = 0
-    for line in readme:
-        result.append(line)
-        # find year
-        match = re.findall(r'## Year (\d+)', line)
-        #print(match, line)
-        if match:
-            year = int(match[0])
-            random.Random(year).shuffle(X_MAS_EMOJIES)
-            readme_year = current_year == year
-            print(f"Year {year}")
-        # create/find day
-        match = re.findall(r'### . Day (\d+)', line)
-        if match:
-            readme_day = int(match[0])
-        # add new timing
-        #print(readme_year, readme_day)
-        if (line == '<hr>' or last_day_known == 0) and readme_year and readme_day == last_day_known:
-            for day in sorted(days_available):
-                result.append('')
-                print(f"Updating day {day} {current_year}")
-                # get day's name
-                emoji = X_MAS_EMOJIES[day % len(X_MAS_EMOJIES)]
-                result.append(f"### {emoji} Day {day}\n")
-                # Get each part
-                for part in range(1,2+1):
-                    result.append(f"#### Day {day} Solution Part {part}\n")
-                    day_path = os.path.join(root_path, str(current_year), f"day_{day}")
-                    solution_file = os.path.join(day_path, f"solution_part_{part}.py")
-                    input_file = os.path.join(day_path, f"input.txt")
-                    # Perform the run
-                    run_output = check_output(["python3", solution_file, input_file])
-                    run_output = run_output.decode('utf-8').strip()
-                    result += run_output.splitlines()[-2:]
-                    # Add the approach
-                    with open(solution_file, 'r') as f:
-                        code_text = f.read()
-                    code_text = code_text[:code_text.find('if __name__ == "__main__":')].strip()
-                    result.append("\n\n```python")
-                    result += code_text.splitlines()
-                    result.append("```")
-                result.append('<hr>')
-            last_day_known = -1
-    result.append('')
-    
-    with open(readme_file, 'w') as f:
-        f.write('\n'.join(result))
+    main(readme_template_name='readmetemplate.md')
