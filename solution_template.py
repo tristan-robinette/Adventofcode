b8b41076999ae2a525c687ce086c151b4fa53930
tristan-robinette <tristan-robinette@hotmail.ca>
2023-12-02 15:04:55 +0000

    spaces -> tabs lol

--- solution_template.py
+++ solution_template.py
@@ -26,9 +26,9 @@ def solution(input_file):
 
 
 if __name__ == "__main__":
-    input_file = sys.argv[1] if len(sys.argv)>1 else 'input.txt'
-    start = time.time()
-    answer = solution(input_file)
-    solution_time = time.time() - start
-    print(f"- **Answer**: {answer}")
-    print(f"- **Timing**: {solution_time}")
+	input_file = sys.argv[1] if len(sys.argv)>1 else 'input.txt'
+	start = time.time()
+	answer = solution(input_file)
+	solution_time = time.time() - start
+	print(f"- **Answer**: {answer}")
+	print(f"- **Timing**: {solution_time}")
