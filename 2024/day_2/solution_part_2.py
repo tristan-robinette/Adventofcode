58259eb7e73c8dd95e9eafcd59d70ef54fac9a29
tristan-robinette <tristan-robinette@hotmail.ca>
2024-12-02 11:23:32 +0000

    new levels

---
+++ 2024/day_2/solution_part_2.py
@@ -0,0 +1,41 @@
+"""
+Solution to Advent of Code 2024 day 2 part 1
+Solved by doing some magic
+"""
+import time
+import sys
+
+
+def safe(line):
+	return (
+			sorted(line) in [line, line[::-1]] and
+			all(1 <= abs(a - b) <= 3 for a, b in zip(line, line[1:]))
+	)
+
+
+def solution(input_file):
+	with open(input_file, 'r') as file:
+		entries = file.read()
+	entries = entries.strip()
+	# Parsing
+	sol = 0
+	entries = entries.splitlines()
+	for line in entries:
+		line = [int(c) for c in line.split()]
+		can_add = False
+		for j in range(len(line)):
+			l2 = line[:j] + line[j + 1:]
+			if safe(l2):
+				can_add = True
+		if can_add:
+			sol += 1
+	return sol
+
+
+if __name__ == "__main__":
+	input_file = sys.argv[1] if len(sys.argv) > 1 else 'input1.txt'
+	start = time.time()
+	answer = solution(input_file)
+	solution_time = time.time() - start
+	print(f"- **Answer**: {answer}")
+	print(f"- **Timing**: {solution_time}")
